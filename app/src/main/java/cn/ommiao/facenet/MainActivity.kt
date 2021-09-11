cd448f65a1d1adb287414ef8437097416a1fbbd2
Kai Li <ommiao@foxmail.com>
2021-09-11 19:19:56 +0800

    Add camerax preview.

--- app/src/main/java/cn/ommiao/facenet/MainActivity.kt
+++ app/src/main/java/cn/ommiao/facenet/MainActivity.kt
@@ -3,6 +3,10 @@ package cn.ommiao.facenet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -11,12 +15,15 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -26,6 +33,8 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import cn.ommiao.facenet.extension.expandFraction
 import cn.ommiao.facenet.ui.theme.FaceNetTheme
@@ -56,13 +65,7 @@ class MainActivity : ComponentActivity() {
                         SheetContent(sheetPeekHeight, scaffoldState.expandFraction)
                     }
                 ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(color = Color.Gray)
-                    ) {
-                        Text(text = "Content of Sheet", modifier = Modifier.align(Alignment.Center))
-                    }
+                    CameraPreview()
                 }
             }
         }
@@ -196,5 +199,39 @@ class MainActivity : ComponentActivity() {
                 contentScale = ContentScale.Crop
             )
         }
+    }
+
+
+    @Composable
+    fun CameraPreview() {
+        val lifecycleOwner = LocalLifecycleOwner.current
+        val context = LocalContext.current
+        val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+
+        AndroidView(
+            factory = { ctx ->
+                val previewView = PreviewView(ctx)
+                val executor = ContextCompat.getMainExecutor(ctx)
+                cameraProviderFuture.addListener({
+                    val cameraProvider = cameraProviderFuture.get()
+                    val preview = Preview.Builder().build().also {
+                        it.setSurfaceProvider(previewView.surfaceProvider)
+                    }
+
+                    val cameraSelector = CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build()
+
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        preview
+                    )
+                }, executor)
+                previewView
+            },
+            modifier = Modifier.fillMaxSize(),
+        )
     }
 }
